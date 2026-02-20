b6c7f484146fc64a60b145ccb52c0b40a92d710f
Mvntando <mvntando@gmail.com>
2026-02-20 09:44:25 +0200

    Refactor search algorithm: streamline iterative deepening and move scoring; enhance alpha-beta and quiescence functions

--- search.py
+++ search.py
@@ -2,12 +2,10 @@
 # iterative deepening
 # quiescence
 # move ordering
-# transposition table (later)
-
-# search.py
+# transposition table
 
 from dorse import Position, Move
-from evaluate import evaluate
+from evaluate import evaluate, PIECE_VALUES
 import helpers
 
 INF = 1000000
@@ -29,37 +27,48 @@ def search(position: Position, depth: int | None = None) -> Move | None: # TODO:
     if depth is None:
         depth = MAX_DEPTH
 
-    for i in range(MAX_DEPTH):  # Clear killer moves for new search
+    for i in range(depth):  # Clear killer moves for new search
         KILLERS[i][0] = None
         KILLERS[i][1] = None
 
-    best_move = None
-    alpha = -INF
-    beta = INF
+    best_move: Move | None = None
 
-    moves = position.gen_moves()
-    order(moves, 0)
-    for i in range(len(moves)):
-        index = i  # best move index
-        for j in range(i + 1, len(moves)):
-            if moves[j].score > moves[index].score:
-                index = j
+    for c_depth in range(1, depth + 1):  # Iterative deepening
+        s_nodes = Nodes  # nodes at start of this iteration
+        alpha = -INF
+        beta = INF
 
-        moves[i], moves[index] = moves[index], moves[i]
+        l_best = None  # local best move for this iteration
+        moves = position.gen_moves()
+        score_moves(moves, 0)
 
-        move = moves[i]
-        mover = position.sd
-        position.push(move)
-        if position.in_check(mover):  # Illegal move, skip
+        if best_move is not None:  # try best_move first from previous iteration
+            for m in moves:
+                if m == best_move:
+                    m.score = 10_000_000  # force first
+                    break
+
+        moves.sort(key=lambda m: m.score, reverse=True)
+
+        for move in moves:
+            mover = position.sd
+            position.push(move)
+            if position.in_check(mover):
+                position.pop()
+                continue
+
+            score = -alphabeta(position, -beta, -alpha, c_depth - 1, 1)
             position.pop()
-            continue
 
-        score = -alphabeta(position, -beta, -alpha, depth-1, 1)
-        position.pop()
+            if l_best is None or score > alpha:
+                alpha = score
+                l_best = move
 
-        if best_move is None or score > alpha:
-            alpha = score
-            best_move = move
+        if l_best is not None:
+            best_move = l_best
+
+        print(f"info depth {c_depth} nodes {Nodes - s_nodes} score {alpha}")
+
 
     print(f"Nodes: {Nodes}")
     return best_move
@@ -76,16 +85,10 @@ def alphabeta(position: Position, alpha: int, beta: int, depth: int, ply: int) -
 
     found = False
     moves = position.gen_moves()
-    order(moves, ply)
-    for i in range(len(moves)):
-        index = i  # best move index
-        for j in range(i + 1, len(moves)):
-            if moves[j].score > moves[index].score:
-                index = j
 
-        moves[i], moves[index] = moves[index], moves[i]
-
-        move = moves[i]
+    score_moves(moves, ply)
+    moves.sort(key=lambda m: m.score, reverse=True)
+    for move in moves:
         mover = position.sd
         position.push(move)
         if position.in_check(mover):  # Illegal move, skip
@@ -93,7 +96,7 @@ def alphabeta(position: Position, alpha: int, beta: int, depth: int, ply: int) -
             continue
 
         found = True
-        score = -alphabeta(position, -beta, -alpha, depth-1, ply+1)
+        score = -alphabeta(position, -beta, -alpha, depth - 1, ply + 1)
         position.pop()
 
         if score >= beta:
@@ -108,7 +111,7 @@ def alphabeta(position: Position, alpha: int, beta: int, depth: int, ply: int) -
     # Mate / stalemate detection after all moves
     if not found:
         if position.in_check(position.sd):
-            return -MATE-depth  # mate TODO: change depth to ply
+            return -MATE + ply  # mate
         else:
             return 0  # stalemate
 
@@ -128,23 +131,16 @@ def quiescence(position: Position, alpha: int, beta: int, ply: int = 0) -> int: 
         alpha = score
     
     moves = position.gen_captures()
-    order(moves, ply)
-    for i in range(len(moves)):
-        index = i  # best move index
-        for j in range(i + 1, len(moves)):
-            if moves[j].score > moves[index].score:
-                index = j
-
-        moves[i], moves[index] = moves[index], moves[i]
-
-        move = moves[i]
+    score_moves(moves, ply)
+    moves.sort(key=lambda m: m.score, reverse=True)
+    for move in moves:
         mover = position.sd
         position.push(move)
         if position.in_check(mover):  # Illegal move, skip
             position.pop()
             continue
 
-        score = -quiescence(position, -beta, -alpha, ply+1)
+        score = -quiescence(position, -beta, -alpha, ply + 1)
         position.pop()
 
         if score >= beta:
@@ -155,20 +151,17 @@ def quiescence(position: Position, alpha: int, beta: int, ply: int = 0) -> int: 
     return alpha
 
 # Score moves using MVV-LVA and store in move.score
-PIECE_VALUE = {
-    'P': 100, 'N': 320, 'B': 330, 'R': 500, 'Q': 900, 'K': 20000,
-    'p': 100, 'n': 320, 'b': 330, 'r': 500, 'q': 900, 'k': 20000
-}
-def order(moves: list[Move], depth: int) -> None:
+PIECE_VALUE = {p: abs(v) for p, v in PIECE_VALUES.items()}
+def score_moves(moves: list[Move], depth: int) -> None:
     for move in moves:
         move.score = 0
         if move.captured is not None:
             attacker = move.piece
             victim = move.captured
-            move.score = 100000 + PIECE_VALUE[victim] * 10 - PIECE_VALUE[attacker]
+            move.score = 100_000 + PIECE_VALUE[victim] * 10 - PIECE_VALUE[attacker]
 
         else:
             if KILLERS[depth][0] is not None and move == KILLERS[depth][0]:
-                move.score = 90000
+                move.score = 90_000
             elif KILLERS[depth][1] is not None and move == KILLERS[depth][1]:
-                move.score = 80000
+                move.score = 80_000
